3d189f90d18c8001430634bff9b37e74d43d99cf
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-09-25 18:19:11 +0545

    create profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from django.contrib.auth import get_user_model
+from .models import UserProfile
 # Register your models here.
 
 @admin.register(get_user_model())
@@ -11,3 +12,8 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['username','email','is_staff']
     fieldsets = UserAdmin.fieldsets + ((None,{'fields':('age','nickname',)}),)
     add_fieldsets = UserAdmin.add_fieldsets + ((None,{'fields':('age','nickname',)}),)
+
+
+
+
+admin.site.register(UserProfile)
--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(blank = True, null=True)
     nickname = models.CharField(max_length=20, blank=True, null=True)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='profile_avatar', blank=True, null=True)
+    body = models.TextField()
+    address = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.last_name}"
